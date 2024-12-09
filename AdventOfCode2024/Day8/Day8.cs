1e361ce65c1d872f2d981283a2ed7e01aa4dc61b
Jedrulcia <jedrzej.koriat@gmail.com>
2024-12-09 18:57:36 +0100

    day 8 part2 now working

--- AdventOfCode2024/Day8/Day8.cs
+++ AdventOfCode2024/Day8/Day8.cs
@@ -115,116 +115,124 @@ namespace AdventOfCode2024
 			return counter;
 		}
 
+		//1267
+		//1272
+		//1283
+		//1287
 		public static int Part2()
-		{
-			Day8[,] tab = GetObjectArray();
-			char[,] tabChar = ConvertFromTxtToArray();
-			bool shouldContinue = true;
-			int counter = 0;
+        {
+            Day8[,] tab = GetObjectArray();
+            char[,] tabChar = ConvertFromTxtToArray();
+            int counter = 0;
 
-			while (shouldContinue)
-			{
-				shouldContinue = false;
-				for (int i = 0; i < tab.GetLength(0); i++)
-				{
-					for (int j = 0; j < tab.GetLength(1); j++)
-					{
-						if (tab[i, j].Value != '.' || tabChar[i, j] == '#')
-						{
-							bool firstLoop = true;
-							for (int k = i; k < tab.GetLength(0); k++)
-							{
-								for (int l = 0; l < tab.GetLength(1); l++)
-								{
-									if (firstLoop)
-									{
-										if (j + 1 == tab.GetLength(1) && k + 1 < tab.GetLength(0))
-										{
-											k++;
-										}
-										else if (j + 1 > tab.GetLength(1))
-										{
-											l = j + 1;
-										}
-										firstLoop = false;
-									}
-									if (tab[i, j].Value == tab[k, l].Value || tabChar[k, l] == '#')
-									{
-										int differenceI = 0;
-										int differenceJ = 0;
+            for (int i = 0; i < tab.GetLength(0); i++)
+            {
+                for (int j = 0; j < tab.GetLength(1); j++)
+                {
+                    if (tab[i, j].Value != '.')
+                    {
+                        bool firstLoop = true;
+                        for (int k = i; k < tab.GetLength(0); k++)
+                        {
+                            for (int l = 0; l < tab.GetLength(1); l++)
+                            {
+                                if (firstLoop)
+                                {
+                                    if (j + 1 == tab.GetLength(1))
+                                    {
+                                        k++;
+                                    }
+                                    else
+                                    {
+                                        l = j + 1;
+                                    }
+                                    firstLoop = false;
+                                }
+                                if (tab[i, j].Value == tab[k, l].Value)
+                                {
+                                    if (tab[i, j].PlacedAntinode == false)
+                                    {
+                                        counter++;
+                                        tab[i, j].PlacedAntinode = true;
+                                        tabChar[i, j] = '#';
+                                    }
+                                    if (tab[k, l].PlacedAntinode == false)
+                                    {
+                                        counter++;
+                                        tab[k, l].PlacedAntinode = true;
+                                        tabChar[k, l] = '#';
+                                    }
 
-										int antinodeFirstI = 0;
-										int antinodeFirstJ = 0;
+                                    int differenceI = 0;
+                                    int differenceJ = 0;
 
-										int antinodeSecondI = 0;
-										int antinodeSecondJ = 0;
+                                    int antinodeFirstI = i;
+                                    int antinodeFirstJ = j;
 
-										if (k > i)
-										{
-											differenceI = k - i;
-											antinodeFirstI = i - differenceI;
-											antinodeSecondI = k + differenceI;
-										}
-										else
-										{
-											antinodeFirstI = i;
-											antinodeSecondI = i;
-										}
-										if (j > l)
-										{
+                                    int antinodeSecondI = k;
+                                    int antinodeSecondJ = l;
+
+                                    bool shouldContinue = true;
+									while(shouldContinue)
+                                    {
+                                        shouldContinue = false;
+                                        if (k > i)
+                                        {
+                                            differenceI = k - i;
+                                            antinodeFirstI -= differenceI;
+                                            antinodeSecondI += differenceI;
+                                        }
+                                        if (j > l)
+                                        {
 											differenceJ = j - l;
-											antinodeFirstJ = j + differenceJ;
-											antinodeSecondJ = l - differenceJ;
-										}
-										else if (l > j)
-										{
-											differenceJ = l - j;
-											antinodeFirstJ = j - differenceJ;
-											antinodeSecondJ = l + differenceJ;
-										}
-										else
-										{
-											antinodeFirstJ = j;
-											antinodeSecondJ = j;
-										}
+                                            antinodeFirstJ += differenceJ;
+                                            antinodeSecondJ -= differenceJ;
+                                        }
+                                        else if (l > j)
+                                        {
+                                            differenceJ = l - j;
+                                            antinodeFirstJ -= differenceJ;
+                                            antinodeSecondJ += differenceJ;
+                                        }
 
-										if (antinodeFirstI >= 0 && antinodeFirstI < tab.GetLength(0) && antinodeFirstJ >= 0 && antinodeFirstJ < tab.GetLength(1))
-										{
-											if (tab[antinodeFirstI, antinodeFirstJ].PlacedAntinode == false)
-											{
-												counter++;
-												tab[antinodeFirstI, antinodeFirstJ].PlacedAntinode = true;
-												tabChar[antinodeFirstI, antinodeFirstJ] = '#';
-												shouldContinue = true;
-											}
-										}
+                                        if (antinodeFirstI >= 0 && antinodeFirstI < tab.GetLength(0) && antinodeFirstJ >= 0 && antinodeFirstJ < tab.GetLength(1))
+                                        {
+                                            shouldContinue = true;
+                                            if (tab[antinodeFirstI, antinodeFirstJ].PlacedAntinode == false)
+                                            {
+                                                counter++;
+                                                tab[antinodeFirstI, antinodeFirstJ].PlacedAntinode = true;
+                                                tabChar[antinodeFirstI, antinodeFirstJ] = '#';
+                                            }
+                                        }
 
-										if (antinodeSecondI >= 0 && antinodeSecondI < tab.GetLength(0) && antinodeSecondJ >= 0 && antinodeSecondJ < tab.GetLength(1))
-										{
-											if (tab[antinodeSecondI, antinodeSecondJ].PlacedAntinode == false)
-											{
-												counter++;
-												tab[antinodeSecondI, antinodeSecondJ].PlacedAntinode = true;
-												tabChar[antinodeSecondI, antinodeSecondJ] = '#';
-												shouldContinue = true;
-											}
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-			return counter;
-		}
+                                        if (antinodeSecondI >= 0 && antinodeSecondI < tab.GetLength(0) && antinodeSecondJ >= 0 && antinodeSecondJ < tab.GetLength(1))
+                                        {
+                                            shouldContinue = true;
+                                            if (tab[antinodeSecondI, antinodeSecondJ].PlacedAntinode == false)
+                                            {
+                                                counter++;
+                                                tab[antinodeSecondI, antinodeSecondJ].PlacedAntinode = true;
+                                                tabChar[antinodeSecondI, antinodeSecondJ] = '#';
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+			PrintArray(tabChar);
+            return counter;
+        }
 
-		private static Day8[,] GetObjectArray()
+        private static Day8[,] GetObjectArray()
 		{
 			string basePath = AppDomain.CurrentDomain.BaseDirectory;
 			string projectPath = Path.GetFullPath(Path.Combine(basePath, @"..\..\..\"));
 			string dayPath = Path.Combine(projectPath, "Day8");
-			string filePath = Path.Combine(dayPath, "Day8test.txt");
+			string filePath = Path.Combine(dayPath, "Day8.txt");
 
 			var lines = File.ReadAllLines(filePath);
 
@@ -246,7 +254,7 @@ namespace AdventOfCode2024
 			string basePath = AppDomain.CurrentDomain.BaseDirectory;
 			string projectPath = Path.GetFullPath(Path.Combine(basePath, @"..\..\..\"));
 			string dayPath = Path.Combine(projectPath, "Day8");
-			string filePath = Path.Combine(dayPath, "Day8test.txt");
+			string filePath = Path.Combine(dayPath, "Day8.txt");
 
 			var lines = File.ReadAllLines(filePath);
 
